4586149cd6b995428fdcfe43e85a0f32033b589e
CodeSandbox <support@codesandbox.io>
2024-03-18 07:07:39 +0000

    Initial commit

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
-app.use("/users", usersRouter);
+app.use("/", usersRouter);
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
--- routes/users.js
+++ routes/users.js
@@ -6,4 +6,38 @@ router.get("/", function (req, res, next) {
   res.send("respond with a resource");
 });
 
+router.post("/bfhl", (req, res) => {
+  const { data } = req.body;
+
+  const numArr = [];
+  const strArr = [];
+
+  data.forEach((d) => {
+    if (typeof d === "number") {
+      numArr.push(d);
+    }
+
+    if (typeof d === "string") {
+      strArr.push(d.toUpperCase());
+    }
+  });
+
+  const odd = numArr.filter((num) => num % 2 != 0);
+  const even = numArr.filter((num) => num % 2 == 0);
+
+  console.log(numArr);
+  console.log(strArr);
+  console.log(odd);
+  console.log(even);
+
+  res.send({
+    is_success: true,
+    user_id: "Akash_20072002",
+    email: "akash1630.be21@chitkara.edu.in",
+    roll_number: "2110991630",
+    odd_numbers: odd,
+    even_numbers: even,
+    alphabets: strArr,
+  });
+});
 module.exports = router;
