f0ef86119666edbe4f968b6dcfc36c1147c2308d
Akash <101383490+Akashx1550@users.noreply.github.com>
2024-03-18 14:40:14 +0530

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
-app.use("/", usersRouter);
+app.use("/temp", usersRouter);
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
