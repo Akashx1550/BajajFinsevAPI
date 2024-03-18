b325db3ea86d70c03c16ea31495c025763fe3692
Akash <101383490+Akashx1550@users.noreply.github.com>
2024-03-18 14:39:46 +0530

    Update users.js

--- routes/users.js
+++ routes/users.js
@@ -2,13 +2,10 @@ var express = require("express");
 var router = express.Router();
 
 /* GET users listing. */
-router.get("/", function (req, res, next) {
-  res.send("respond with a resource");
-});
 
 router.post("/bfhl", (req, res) => {
   const { data } = req.body;
-
+  
   const numArr = [];
   const strArr = [];
 
